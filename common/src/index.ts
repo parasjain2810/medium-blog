951fe2e3dd4c3cdc4f5c9834aea2d2783871c80a
parashjain2810 <parashjain2810@gmail.com>
2024-08-03 14:14:00 +0530

    npm package common okey

---
+++ common/src/index.ts
@@ -0,0 +1,27 @@
+import  z from "zod";
+
+export const signupInput=z.object({
+    email:z.string().email(),
+    password:z.string(),
+    name:z.string().optional(),
+})
+
+export const signinInput=z.object({
+    email:z.string().email(),
+    password:z.string(),
+})
+
+export const createPostInput=z.object({
+    title:z.string(),
+    content:z.string(),
+})
+
+export const updatePostInput=z.object({
+    title:z.string().optional(),
+    content:z.string().optional(),
+})
+
+export type SigninInput=z.infer<typeof signinInput>;
+export type CreatePostInput=z.infer<typeof createPostInput>
+export type SignupInput=z.infer<typeof signupInput>;
+export type UpdatePostInput=z.infer<typeof updatePostInput>;
